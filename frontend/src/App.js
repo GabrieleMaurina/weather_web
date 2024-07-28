aabc551068018eef02daf5e6271062189029250c
GabrieleMaurina <gabrielemaurina95@gmail.com>
2024-07-28 17:06:39 -0600

    Added bootstrap.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 function App() {
-  return <p>Hello World!</p>;
+  return (
+    <Navbar bg="primary" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default App;
